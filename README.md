d5c291b4ead8c3e92face360bad02e9378248ccb
olopcodes <olopcodes@gmail.com>
2022-01-22 22:04:21 -0500

    my first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# A Blackjack Game
+
+---
+
+A simple blackjack game with only one player. Some functionality of blackjack have not been included: doubling down, splitting pairs, insurance, or surrender. Winning by getting 21 or as close as you can to 21 before the other folds
+
+## How to Play
+
+---
+
+- Press Start Game
+- Enter Your Name in the prompt
+- Place your bets (bets increment and decrement by 5)
+- Click draw to get your card
+- Click hit to draw another card
+- Click fold when you do not want another card
+
+## Technology used
+
+---
+
+- HTML
+- CSS
+- JS
+
+## Features
+
+---
+
+- Use of localstorage
+- Fetching data from an api
+
+## Credits
+
+---
+
+This project began as part of the Scrimba Frontend Career Path
---
+++ index.js
@@ -0,0 +1,110 @@
+// use local storage and set the player object
+
+async function getDecks() {
+  const res = await fetch(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
+  );
+  const data = await res.json();
+  return data.deck_id;
+}
+
+// the cards from here should stored in the cards array
+async function getCards(num) {
+  const deckId = await getDecks();
+  const res = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`
+  );
+  const data = await res.json();
+  console.log(data);
+}
+
+getCards(2);
+getCards(1);
+
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+
+const playerEl = document.getElementById("player-el");
+const cardsEl = document.getElementById("cards-el");
+const sumEl = document.getElementById("sum-el");
+const messageEl = document.getElementById("message-el");
+const player = {
+  name: "",
+  chips: 150,
+
+  updatePlayerName() {
+    // check if info in localStorage, if not run prompt
+    this.name = prompt("Enter your name");
+  },
+
+  printName() {
+    console.log(this.name);
+  },
+
+  renderPlayerInfo() {},
+};
+
+function startGame() {
+  //   as long as player chips > 0
+  isAlive = true;
+  //   player.updatePlayerName();
+  //   player.printName();
+  cards = [];
+  let firstCard = getRandomCard();
+  cards.push(firstCard);
+  let secondCard = getRandomCard();
+  cards.push(secondCard);
+  sum = firstCard + secondCard;
+  renderGame();
+  console.log(cards);
+}
+
+function getRandomCard() {
+  const n = Math.floor(Math.random() * 13) + 1;
+  if (n === 1) {
+    return 11;
+  } else if (n > 10) {
+    return 10;
+  } else {
+    return n;
+  }
+}
+
+function renderCards() {
+  cardsEl.textContent = `Cards: `;
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+}
+
+function renderGame() {
+  renderCards();
+
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  // as long as player is still alive and has not got blackjack
+
+  if (isAlive && !hasBlackJack) {
+    const card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
+}
