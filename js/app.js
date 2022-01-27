60ee73fa9d94568f74012c34d48444797ae3d60e
olopcodes <olopcodes@gmail.com>
2022-01-26 21:52:03 -0500

    starting the build from scratch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-const startBtn = document.getElementById("start-btn");
-let game;
+// const startBtn = document.getElementById("start-btn");
+// const betBtns = document.querySelector("#blackjack-bet-btns");
+// const playerEl = document.getElementById("player-el");
+// let game;
 
 // get a new deck of cards from the api
-startBtn.addEventListener("click", async (e) => {
-  startBtn.classList.add("close");
-  const id = await getDeckId();
-  game = new Game(id);
-});
+// startBtn.addEventListener("click", async (e) => {
+//   startBtn.classList.add("close");
+//   betBtns.classList.remove("close");
+//   playerEl.classList.remove("close");
+//   const id = await getDeckId();
+//   game = new Game(id);
+// });
 
 // this should run when the bet button is pressed
 // const cards = await drawCards(game.deckId, 4);
